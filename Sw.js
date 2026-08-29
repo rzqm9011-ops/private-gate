56ceb2b3b7bbb3e7b60234acb8401d694a21449a
rzqm9011-ops <rzqm9011@gmail.com>
2026-08-29 03:58:51 +0300

    Add files via upload

---
+++ Sw.js
@@ -0,0 +1,7 @@
+
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => {
+  // simple cache-first for offline, but don't break
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
