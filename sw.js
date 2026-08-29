3c6b7bc540aaa193748fa8707c8561a53c26374f
rzqm9011-ops <rzqm9011@gmail.com>
2026-08-29 04:51:17 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
-self.addEventListener('fetch', e => {
-  // simple cache-first for offline, but don't break
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
